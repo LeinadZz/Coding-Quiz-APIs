e727ba8b8fda7cc360580c96d4979ee48a67e6a4
Daniel <leinad6ix.z@gmail.com>
2022-08-24 18:44:58 -0700

    Started on javascript code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -41,54 +41,70 @@ let questions = [
         choice3: 'variables',
         choice4: 'arrays',
         answer: 4, 
-    },
-    {
-        question: 'How do you write an IF statement',
-        choice1: 'if i == 5 then',
-        choice2: 'if i = 5 then',
-        choice3: 'if (i == 5)',
-        choice4: 'if i == 5',
-        answer: 3, 
-    },
-    {
-        question: 'How do you write an IF statement',
-        choice1: 'if i == 5 then',
-        choice2: 'if i = 5 then',
-        choice3: 'if (i == 5)',
-        choice4: 'if i == 5',
-        answer: 3, 
-    },
-    {
-        question: 'How do you write an IF statement',
-        choice1: 'if i == 5 then',
-        choice2: 'if i = 5 then',
-        choice3: 'if (i == 5)',
-        choice4: 'if i == 5',
-        answer: 3, 
-    },
-    {
-        question: 'How do you write an IF statement',
-        choice1: 'if i == 5 then',
-        choice2: 'if i = 5 then',
-        choice3: 'if (i == 5)',
-        choice4: 'if i == 5',
-        answer: 3, 
-    },
-    {
-        question: 'How do you write an IF statement',
-        choice1: 'if i == 5 then',
-        choice2: 'if i = 5 then',
-        choice3: 'if (i == 5)',
-        choice4: 'if i == 5',
-        answer: 3, 
-    },
-    {
-        question: 'How do you write an IF statement',
-        choice1: 'if i == 5 then',
-        choice2: 'if i = 5 then',
-        choice3: 'if (i == 5)',
-        choice4: 'if i == 5',
-        answer: 3, 
-    },
+    }
 ]
 
+var SCORE_POINTS = 100
+const TIME_REMAINING = 0;
+const MAX_QUESTIONS = 4;
+
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion;
+}
+
+function getNewQuestion() {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('./end.html')
+    }
+
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1);
+
+    acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', function (e) {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            SCORE_POINTS++;
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(function () {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+startGame()
